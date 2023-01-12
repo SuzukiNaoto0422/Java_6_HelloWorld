82423e4f9ea3e71625dc4cf29adef1ffd381b3aa
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2023-01-12 15:42:16 +0900

    アクセス修飾子の修正(12行目)

--- src/main/java/com/study/HelloWorld/HelloWorldController.java
+++ src/main/java/com/study/HelloWorld/HelloWorldController.java
@@ -9,7 +9,7 @@ import java.time.LocalTime;
 @RestController
 public class HelloWorldController {
     @RequestMapping("hello")//value属性だけなら省略できる。value = "/hello"
-    String hello() {
+    public String hello() {
         LocalDateTime now =LocalDateTime.now();
         //現在の日時を取得するために LocalDateTime.now()を使用し、現在時刻を取得
         LocalTime localTime = now.toLocalTime();
